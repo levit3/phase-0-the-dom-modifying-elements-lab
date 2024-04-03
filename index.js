521fcfa2897aa566f3389a8939067383989f905c
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-03 11:41:19 +0300

    deletes, creates and adds dom elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Levite is the champion";
+document.body.append(newHeader);
